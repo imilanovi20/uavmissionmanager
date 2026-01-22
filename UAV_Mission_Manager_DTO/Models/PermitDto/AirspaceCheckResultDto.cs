7f0c298596be42da93a83795e2730c5f4b2f5aa8
imilanovi20 <imilanovi20@foi.hr>
2026-01-22 16:39:34 +0100

    Add function to check if route intersects restricted airspace (backend)

---
+++ UAV_Mission_Manager_DTO/Models/PermitDto/AirspaceCheckResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_Mission_Manager_DTO.Models.PermitDto
+{
+    public class AirspaceCheckResultDto
+    {
+        public bool Success { get; set; }
+        public bool CrossesAirspace { get; set; }
+        public string Message { get; set; }
+        public int AirportsViolated { get; set; }
+        public List<AirspaceViolationDto> Violations { get; set; }
+    }
+}
---
+++ UAV_Mission_Manager_DTO/Models/PermitDto/AirspaceViolationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_Mission_Manager_DTO.Models.PermitDto
+{
+    public class AirspaceViolationDto
+    {
+        public string AirportName { get; set; }
+        public double MinDistanceKm { get; set; }
+        public List<int> ViolatingPointIndices { get; set; }
+        public object Type { get; set; }
+        public string Description { get; set; }
+    }
+}
