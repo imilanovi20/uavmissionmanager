41bbcf83fba6206ec53fb3f164827a366d0252ea
imilanovi20 <imilanovi20@foi.hr>
2026-01-26 22:50:44 +0100

    Improve mission persistence in the database (backend)

---
+++ UAV_Mission_Manager_DTO/Models/PathPlanning/CreateObstaclesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_Mission_Manager_DTO.Models.PathPlanning
+{
+    public class CreateObstaclesDto
+    {
+        public int MissionId { get; set; }
+        public List<ObstacleDto> Obstacles { get; set; }
+    }
+}
