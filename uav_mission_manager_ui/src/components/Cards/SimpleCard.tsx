2d9c3b18035ce0bda41f54ec7346e39fba127c1f
imilanovi20 <imilanovi20@foi.hr>
2026-02-06 01:30:01 +0100

    Updated administration of users and uavs (frontend)

--- uav_mission_manager_ui/src/components/Cards/SimpleCard.tsx
+++ uav_mission_manager_ui/src/components/Cards/SimpleCard.tsx
@@ -1,20 +1,26 @@
-import { CardContainer, ContentSection, Description, Section, SectionImage, Subsection } from "./SimpleCard.styles";
+import { Trash2 } from "lucide-react";
+import { CardContainer, ContentSection, Description, DeleteButton, Section, SectionImage, Subsection } from "./SimpleCard.styles";
 import type { SimpleCardProps } from "./SimpleCard.types";
 
-const SimpleCard = (props : SimpleCardProps) => {
-  return (
-    <CardContainer>
-      <SectionImage 
-        src={props.imagePath} 
-        alt={props.altImagePath}
-      />
-      <ContentSection>
-        <Section>{props.sectionTitle}</Section>
-        <Subsection>{props.subsectionTitle}</Subsection>
-        <Description>{props.description}</Description>
-      </ContentSection>
-    </CardContainer>
-  );
+const SimpleCard = (props: SimpleCardProps) => {
+    return (
+        <CardContainer>
+            {props.showDelete && props.onDelete && (
+                <DeleteButton onClick={props.onDelete}>
+                    <Trash2 size={18} />
+                </DeleteButton>
+            )}
+            <SectionImage
+                src={props.imagePath}
+                alt={props.altImagePath}
+            />
+            <ContentSection>
+                <Section>{props.sectionTitle}</Section>
+                <Subsection>{props.subsectionTitle}</Subsection>
+                <Description>{props.description}</Description>
+            </ContentSection>
+        </CardContainer>
+    );
 };
 
 export default SimpleCard;
--- uav_mission_manager_ui/src/pages/Users/UserPage.tsx
+++ uav_mission_manager_ui/src/pages/Users/UserPage.tsx
@@ -3,110 +3,110 @@ import SimpleCard from "../../components/Cards/SimpleCard";
 import { Pagination } from "../../components/Pagination/Pagination";
 import { useCurrentUser } from "../../hooks/useCurrentUser";
 import { useUsers } from "../../hooks/useUsers";
-import { 
-
-  CardContainer, 
-  ErrorMessage, 
-  Header, 
-  HeaderTop, 
-  LoadingSpinner, 
-  PageContainer, 
-  Subtitle, 
-  Title 
+import {
+    CardContainer,
+    ErrorMessage,
+    Header,
+    HeaderTop,
+    LoadingSpinner,
+    PageContainer,
+    Subtitle,
+    Title
 } from "../../components/Containers/CardContainer.styles";
 import { useNavigate } from "react-router-dom";
 
 const UserPage = () => {
-  const defaultImagePath = "/user/default/default-user.jpg";
-  
-  const {
-    paginatedUsers,
-    loading,
-    error,
-    currentPage,
-    totalPages,
-    setCurrentPage,
-    goToNextPage,
-    goToPrevPage
-  } = useUsers({ itemsPerPage: 6 });
-  
-  const { isAdmin } = useCurrentUser();
+    const defaultImagePath = "/user/default/default-user.jpg";
+
+    const {
+        paginatedUsers,
+        loading,
+        error,
+        currentPage,
+        totalPages,
+        setCurrentPage,
+        goToNextPage,
+        goToPrevPage,
+        deleteUser  
+    } = useUsers({ itemsPerPage: 6 });
+
+    const { isAdmin } = useCurrentUser();
     const navigate = useNavigate();
 
-  if (loading) {
+    if (loading) {
+        return (
+            <PageContainer>
+                <LoadingSpinner>Loading users...</LoadingSpinner>
+            </PageContainer>
+        );
+    }
+
+    if (error) {
+        return (
+            <PageContainer>
+                <ErrorMessage>{error}</ErrorMessage>
+            </PageContainer>
+        );
+    }
+
+    const handleAddUser = () => {
+        navigate('/users/add');
+    };
+
+    const handleDeleteUser = async (username: string) => {
+        if (window.confirm(`Are you sure you want to delete user ${username}?`)) {
+            await deleteUser(username);
+        }
+    };
+
     return (
-      
         <PageContainer>
-          <LoadingSpinner>Loading users...</LoadingSpinner>
-        </PageContainer>
-      
-    );
-  }
+            <Header>
+                <HeaderTop>
+                    <div>
+                        <Title>Users</Title>
+                        <Subtitle>System users and administrators</Subtitle>
+                    </div>
+                    {isAdmin && (
+                        <BlackButton
+                            title="+ Add User"
+                            onClick={handleAddUser}
+                            disabled={false}
+                            width="180px"
+                            height="auto"
+                        />
+                    )}
+                </HeaderTop>
+            </Header>
 
-  if (error) {
-    return (
-      
-        <PageContainer>
-          <ErrorMessage>{error}</ErrorMessage>
-        </PageContainer>
-      
-    );
-  }
+            <CardContainer>
+                {paginatedUsers.map((user) => (
+                    <SimpleCard
+                        key={user.username}
+                        imagePath={
+                            user.imagePath === "" || user.imagePath === "/" || !user.imagePath
+                                ? defaultImagePath
+                                : user.imagePath
+                        }
+                        altImagePath={user.username}
+                        sectionTitle={user.username}
+                        subsectionTitle={user.role}
+                        description={user.email || "No email provided"}
+                        showDelete={isAdmin}  
+                        onDelete={() => handleDeleteUser(user.username)}  
+                    />
+                ))}
+            </CardContainer>
 
-  const handleAddUser = () => {
-    navigate('/users/add');
-  };
-
-  return (
-    
-      <PageContainer>
-        <Header>
-          <HeaderTop>
-            <div>
-              <Title>Users</Title>
-              <Subtitle>System users and administrators</Subtitle>
-            </div>
-            {isAdmin && (
-
-              <BlackButton
-                title="+ Add User"
-                onClick={handleAddUser}
-                disabled={false}
-                width="180px"
-                height="auto"
-              />
-
-            )}
-          </HeaderTop>
-        </Header>
-
-        <CardContainer>
-          {paginatedUsers.map((user) => (
-            <SimpleCard
-              key={user.username}
-              imagePath={
-                user.imagePath === "" || user.imagePath === "/" || !user.imagePath
-                  ? defaultImagePath
-                  : user.imagePath
-              }
-              altImagePath={user.username}
-              sectionTitle={user.username}
-              subsectionTitle={user.role}
-              description={user.email || "No email provided"}
+            <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={setCurrentPage}
+                onPrevious={goToPrevPage}
+                onNext={goToNextPage}
             />
-          ))}
-        </CardContainer>
-       
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={setCurrentPage}
-          onPrevious={goToPrevPage}
-          onNext={goToNextPage}
-        />
-      </PageContainer>
-    
-  );
+        </PageContainer>
+    );
 };
 
 export default UserPage;
